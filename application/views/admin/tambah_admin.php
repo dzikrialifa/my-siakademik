098effe5d6334ea27066f400fa9a601a14c053f8
dzikrialifa <dzikrialif99@gmail.com>
2019-03-19 14:14:29 +0700

    Templating Admin

--- application/views/admin/tambah_admin.php
+++ application/views/admin/tambah_admin.php
@@ -1,54 +1,81 @@
-<div class="row">
+<!DOCTYPE html>
+<html lang="en">
 
-    <!-- Jumlah IPA -->
-    <div class="col-xl-4 col-md-6 mb-4">
-        <div class="card border-left-danger shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-md font-weight-bold text-primary text-uppercase mb-1">Jumlah Siswa IPA</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">150 Orang</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Register</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="<?= base_url('assets_login'); ?>/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assets_login/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary mt-5">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5 col-lg-6 mx-auto">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form class="user">
+
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                                    </div>
+                                </div>
+                                <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                    Register Account
+                                </a>
+
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="login.html">Already have an account? Login!</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 
-    <!-- Jumlah IPS -->
-    <div class="col-xl-4 col-md-6 mb-4">
-        <div class="card border-left-danger shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-md font-weight-bold text-primary text-uppercase mb-1">Jumlah Siswa IPS</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">150 Orang</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?= base_url('assets_login/'); ?> vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('assets_login/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Permintaan Reset Password  -->
-    <div class="col-xl-4 col-md-6 mb-4">
-        <div class="card border-left-danger shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-md font-weight-bold text-danger text-uppercase mb-1">Permintaan Reset Password</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">Tidak Ada</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-undo-alt fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- Core plugin JavaScript-->
+    <script src="<?= base_url('assets_login/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
-</div> 
+    <!-- Custom scripts for all pages-->
+    <script src="<?= base_url('assets_login/'); ?>js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html> 
