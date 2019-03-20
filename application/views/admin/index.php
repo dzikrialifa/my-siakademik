9ccfeb44805fb11bdd517659de0328d119d54eef
dzikrialifa <dzikrialif99@gmail.com>
2019-03-20 21:40:13 +0700

    Templating Admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -39,7 +39,7 @@
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">dz Luna</span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">dasd</span>
                         <img class="img-profile rounded-circle" src="<?= base_url('assets_login/img/'); ?>admin.jpg">
                     </a>
                     <!-- Dropdown - User Information -->
