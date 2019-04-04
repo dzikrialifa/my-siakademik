65460ce5299d0955dfe1126fead11ac75a9d6b15
dzikrialifa <dzikrialif99@gmail.com>
2019-04-04 09:23:42 +0700

    Templating Home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -42,13 +42,41 @@
     <!-- Content -->
 
     <!-- jumbotron -->
-    <div class="container">
+    <!-- <div class="container">
         <div class="jumbotron">
             <h1 class="display-4">Hello, world!</h1>
             <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr class="my-4">
             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+    </div> -->
+    <div class="container-fluid">
+        <div id="demo" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="<?= base_url('picture/pict01.jpg') ?>" class="d-block" style="width:1500px;height:600px" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= base_url('picture/pict02.jpg') ?>" class="d-block" style="width:1500px;height:600px" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= base_url('picture/pict03.jpg') ?>" class="d-block" style="width:1500px;height:600px" alt="...">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#demo" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#demo" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 
@@ -315,7 +343,7 @@
         </section>
     </div>
     <!-- Javascript dan JQuery -->
-    <script src="<?= base_url('bootstrap/') ?>>dist/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="<?= base_url('bootstrap/') ?>dist/js/jquery-3.3.1.slim.min.js"></script>
     <script src="<?= base_url('bootstrap/') ?>dist/js/popper.min.js"></script>
     <script src="<?= base_url('bootstrap/') ?>dist/js/bootstrap.min.js"></script>
 </body>
